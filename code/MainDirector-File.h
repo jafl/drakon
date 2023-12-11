938d9d4cd9b8de23f8842634bd275ea3da1ac60d
John Lindal <github@newplanetsoftware.com>
2023-12-10 21:10:55 -0800

    add windows shortcuts

--- code/MainDirector-File.h
+++ code/MainDirector-File.h
@@ -15,7 +15,7 @@ enum {
 
 
 static void ConfigureFileMenu(JXTextMenu* menu, const int offset = 0) {
-	if (menu->IsEmpty() && JXMenu::GetDisplayStyle() == JXMenu::kWindowsStyle) {
+	if (offset == 0 && JXMenu::GetDisplayStyle() == JXMenu::kWindowsStyle) {
 		menu->SetShortcuts(JString("#f", JString::kNoCopy));
 	}
 };
